d8dd985f7b4d9f81ea8ce81b399191f4e46bd1c4
Juan-Glitch <60455275+Juan-glitch@users.noreply.github.com>
2021-08-31 16:00:19 +0200

    Update test_streamlit.py
    Cambios con nuevos añadidos con propiedades de texto.

--- test_streamlit.py
+++ test_streamlit.py
@@ -1,4 +1,9 @@
-import streamlit
+import streamlit as st
 
-streamlit.write("HELLO WORLD")
-streamlit.checkbox("Checkbox")
+st.markdown("HELLO WORLD") # Titulo
+st.markdown("__Con esto en negrita__") # Titulo pero en black
+st.write("texto simple") # Texto simple 
+st.write("__Texto simple en negrita__") # Texto simple en negrita 
+st.markdown(":+1:") # Emoticonos?¿
+st.markdown("<center>hello en html con propiedades</center>", True)
+streamlit.checkbox("Checkbox", help = "Información adicional")
