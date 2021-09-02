b2ad87e73b55dd919cdd1b71cefff3d6a2394c93
Juan-Glitch <60455275+Juan-glitch@users.noreply.github.com>
2021-09-02 12:07:47 +0200

    Update test_streamlit.py
    añadido el slider

--- streamlit-test/test_streamlit.py
+++ streamlit-test/test_streamlit.py
@@ -1,5 +1,6 @@
 import streamlit as st
 
+# Visualizaciones
 st.markdown("HELLO WORLD") # Titulo
 st.markdown("__Con esto en negrita__") # Titulo pero en black
 st.write("texto simple") # Texto simple 
@@ -8,16 +9,22 @@ st.markdown(":+1:") # Emoticonos?¿
 st.markdown("<center>hello en html con propiedades</center>", True)
 st.checkbox("Checkbox", help = "Información adicional")
 
+# Checklist
 if st.checkbox("Cbox interactivo", help = "Si me apretas, saco pecho"):
   """Texto random pero con emoticono :sneezing_face:"""
   """Otro texto pero con otra manera de poner emoticonos &#128520; """
   # Para más emoticonos: https://www.science.co.il/internet/html/Smileys.php
   
-
+# Selector I
 st.selectbox("Esto es un selectbox", ["Valor", "another-one", "Tercer-valor"]) # Encargado de efectuar un selectbox  
 # st.multiselect("Y esto un multiselect", [1,2,3,4], default = [3,4])  # Efectua una selección mutiple
 
-
+# Selector II
 rad = st.radio("Esto es un botón radio", [1,2,3,4],
               format_func = lambda num: "Opcion " + str(num)) # Selecciona un valor y lo guarda para aplicarlo al modelo
 st.write("El valor seleccionado ha sido " + str(rad))
+
+# Slider
+import datetime
+st.slider("Entrega de proyecto", min_value = datetime.date(2021,9,1), max_value = datetime.date(2022,1,2), 
+          value = [datetime.date(2021,9,15), datetime.date(2021,10,1)])
